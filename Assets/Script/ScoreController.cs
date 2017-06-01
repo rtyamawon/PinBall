67c93602203d854940823dcf7d70c6f853369ce2
yamaga <yamaga@wonderleague.co.jp>
2017-06-02 00:24:18 +0900

    Lesson 5 課題提出
    pinboll 課題

---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	// 自分のスコア
+	public int myScore = 0;
+	private int S_StarScore = 10;
+	private int L_StarScore = 20;
+	private int S_CloudScore = 5;
+	private int L_CloudScore = 8;
+     private GameObject scoreText;
+     private Text text;
+        
+	// Use this for initialization
+	void Start () {
+            //シーン中のScoreTextオブジェクトを取得
+            this.scoreText = GameObject.Find("ScoreText");
+            text = this.scoreText.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			//ScoreTextに表示
+            text.text = "SCORE : " + this.myScore;
+	}
+	
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+		//Debug.Log("OnCollisionEnter") ;
+		AddScore(other.gameObject.tag); 
+	}
+	
+	void AddScore(string tag) {
+		 int addScore = 0;
+			if (tag == "SmallStarTag") {
+				addScore = this.S_StarScore;
+			}else if (tag == "LargeStarTag") {
+				addScore = this.L_StarScore;
+			}else if(tag == "SmallCloudTag") {
+				addScore = this.S_CloudScore;
+			}else if(tag == "LargeCloudTag") {
+				addScore = this.L_CloudScore;
+			}            
+			if(addScore > 0) {
+				Debug.Log("Before Score  [" + this.myScore + "]" ) ;
+				Debug.Log("Addscore  [" + addScore  + "]"+ "TAG : " + tag) ;
+				this.myScore += addScore ;
+				Debug.Log("AfterScore  [" + this.myScore + "]" ) ;
+			}
+	}
+	
+}
